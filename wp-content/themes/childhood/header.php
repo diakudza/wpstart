a5ceb62fb2d3d1c4230975aab775fe695a972e0c
diakudza <diakudza@gmail.com>
2022-09-16 16:15:21 +0300

    add history

--- wp-content/themes/childhood/header.php
+++ wp-content/themes/childhood/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Мир Детства</title>
+    <title><?php bloginfo('name'); echo " | "; bloginfo('description'); ?></title>
     <meta name="theme-color" content="#c9e0e04d">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="viewport"
@@ -17,7 +17,6 @@
     <link rel="apple-touch-icon" sizes="120x120" href="<?= bloginfo('template_url'); ?>/assets/img/favicons/apple-touch-icon-120x120.png">
     <link rel="apple-touch-icon" sizes="144x144" href="<?= bloginfo('template_url'); ?>/assets/img/favicons/apple-touch-icon-144x144.png">
     <link rel="apple-touch-icon" sizes="180x180" href="<?= bloginfo('template_url'); ?>/assets/img/favicons/apple-touch-icon-180x180.png">
-    <link rel="stylesheet" href="./styles/main.min.css">
     <?php
         wp_head();
     ?>
@@ -27,22 +26,23 @@
     <div class="container">
         <div class="row align-items-center">
             <div class="col-11 col-sm-12 col-md-12 col-lg-3">
-                <a href="#" class="header__logo">
-                    <img src="<?= bloginfo('template_url'); ?>/assets/img/icons/svg/logo.svg" alt="Мир детства" class="header__logo-img">
-                    <div class="header__logo-text">Мир детства</div>
-                </a>
+                <div class="header__logo">
+                    <?php the_custom_logo(); ?>
+                </div>
             </div>
             <div class="col-md-12 col-lg-9 offset-lg-0 col-xl-7 offset-xl-2 ">
                 <div class="header__contacts">
                     <div class="header__contacts-item">
                         <img src="<?= bloginfo('template_url'); ?>/assets/img/icons/svg/email.svg" alt="почта" class="header__contacts-logo">
-                        <a href="mailto:mirdetstva@gmail.com" class="header__contacts-mail">mirdetstva@gmail.com</a>
+                        <a href="mailto:<?= the_field('mail', 2); ?>" class="header__contacts-mail"><?= the_field('mail', 2); ?></a>
                     </div>
                     <div class="header__contacts-item">
                         <img src="<?= bloginfo('template_url'); ?>/assets/img/icons/svg/phone.svg" alt="телефон" class="header__contacts-logo">
                         <div class="header__contacts-tel">
-                            <a href="tel:+797867834347">+797867834347</a>
-                            <a href="tel:+797867834358">+797867834358</a>
+                            <a href="tel: <?= the_field('tel', 2); ?>"><?= the_field('tel', 2); ?></a>
+                            <?php if (get_field('tel_2', 2)) {?>
+                            <a href="tel: <?= the_field('tel_2', 2); ?>"><?= the_field('tel_2', 2); ?></a>
+                            <?php } ?>
                         </div>
                     </div>
                     <div class="header__contacts-item">
